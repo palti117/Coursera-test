7513093b8bd23c0d4d4122fac6ebec3f9805c9b3
palti117 <ramyapalti123@gmail.com>
2020-05-23 16:58:36 +0530

    final modue

--- javascript/new.js
+++ javascript/new.js
@@ -1,4 +1,7 @@
-var lists=new Array();
-lists[0]="hi";
-lists[1]="ramya";
-console.log(lists);
+function sayhello() {
+	var name=document.getElementById("name").value;
+	var message="<h2>Hello "+name+"!</h2>";
+	document.querySelector("#content").innerHTML=message;
+	document.querySelector("#info").innerHTML="";
+	
+}
